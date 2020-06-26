d39ac34821452ba67355573577fbda9af47a5b13
Dany <danik.kla@mail.ru>
2020-06-26 10:08:12 +0300

    2commit

--- js/java_script.js
+++ js/java_script.js
@@ -1,17 +1,52 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели');
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	generes: [],
-	privet: true
-};
-const a = prompt('Один из последних просмотренных фильмов'),
-	b = prompt('На сколько оцените его?'),
-	c = prompt('Один из последних просмотренных фильмов')
-d = prompt('На сколько оцените его')
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
-console.log(personalMovieDB);
+//const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели');
+//const personalMovieDB = {
+//	count: numberOfFilms,
+//	movies: {},
+//	actors: {},
+//	generes: [],
+//	privet: true
+//};
+//const a = prompt('Один из последних просмотренных фильмов'),
+//	b = prompt('На сколько оцените его?'),
+//	c = prompt('Один из последних просмотренных фильмов')
+//d = prompt('На сколько оцените его')
+//personalMovieDB.movies[a] = b
+//personalMovieDB.movies[c] = d
+//console.log(personalMovieDB);
+
+// if (0) {
+// 	console.log('ok');
+// } else {
+// 	console.log('error');
+
+// }
+// const num = 50;
+// if (num < 39) {
+// 	console.log('error');
+
+// }
+// else if (num > 100) {
+// 	console.log('mnogo');
+
+// }
+// else {
+// 	console.log('ok');
+
+// }
+// (num === 50) ? console.log('ok') : console.log('error');
+const num = '50';
+switch (num) {
+	case '49':
+		console.log('no');
+		break;
+	case '0':
+		console.log('no');
+		break;
+	case '5':
+		console.log('yes');
+		break;
+	default:
+		console.log('слишком много попыток');
 
 
+}
