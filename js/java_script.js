957c584a433840d4a9b1bb879be93277ba037e0e
Dany <danik.kla@mail.ru>
2020-06-27 14:24:48 +0300

    dz3

--- js/java_script.js
+++ js/java_script.js
@@ -34,19 +34,109 @@
 
 // }
 // (num === 50) ? console.log('ok') : console.log('error');
-const num = '50';
-switch (num) {
-	case '49':
-		console.log('no');
-		break;
-	case '0':
-		console.log('no');
-		break;
-	case '5':
-		console.log('yes');
-		break;
-	default:
-		console.log('слишком много попыток');
+// const num = '50';
+// switch (num) {
+// 	case '49':
+// 		console.log('no');
+// 		break;
+// 	case '0':
+// 		console.log('no');
+// 		break;
+// 	case '5':
+// 		console.log('yes');
+// 		break;
+// 	default:
+// 		console.log('слишком много попыток');
 
+
+// }
+let numberOfFilms;
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели');
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели');
+	}
+}
+
+start();
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	generes: [],
+	privat: false
+};
+
+
+
+
+
+function rememberMyFilm() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из последних просмотренных фильмов'),
+			b = prompt('На сколько оцените его?');
+
+		if (a != null && b != null && a != ' ', b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+		} else {
+			console.log('error');
+			i--;
+		}
+	}
+}
+rememberMyFilm();
+if (personalMovieDB.count < 10) {
+	console.log('lox');
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count >= 30) {
+	console.log('norm');
+
+} else if (personalMovieDB.count >= 30) {
+	console.log('kroot');
+
+} else {
+	console.log('error');
 
 }
+function detectPersonalLeval() {
+	if (personalMovieDB.count < 10) {
+		console.log('lox');
+
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count >= 30) {
+		console.log('norm');
+
+	} else if (personalMovieDB.count >= 30) {
+		console.log('kroot');
+
+	} else {
+		console.log('error');
+
+	}
+}
+detectPersonalLeval();
+
+function showMyDB(hidde) {
+	if (!hidde) {
+		console.log(personalMovieDB);
+
+	}
+}
+
+
+
+function writeYourGeners() {
+	for (let k = 1; k <= 3; k++) {
+		let jener = prompt(`Ваш любимый жанр под номером${k}`);
+		personalMovieDB.generes[k - 1] = jener
+		//personalMovieDB.generes[k - 1] = = prompt(`Ваш любимый жанр под номером${k}`);можно так если прикол let только для одной вещи нгужен
+	}
+}
+writeYourGeners(personalMovieDB.generes);
+
+
+
+
+
+showMyDB(personalMovieDB.privat);
